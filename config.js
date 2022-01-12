c3bd7d71d89ef767b70ef28bc9fd3f7203d467f4
Hakucintakamu <97572839+Hakucintakamu@users.noreply.github.com>
2022-01-12 10:14:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "HI Chào Cậu",
-    introTitle: 'Phạm Thị Hải Anh',
+    introTitle: 'Ai Ai Ai',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
