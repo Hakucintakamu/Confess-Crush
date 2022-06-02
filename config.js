2fb142d0cf0100d5cf00c5fd4b173f482055fe79
Hakucintakamu <97572839+Hakucintakamu@users.noreply.github.com>
2022-06-02 20:55:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introTitle: 'chào thằng Tân nguuu',
+    introDesc: `Tại sao Tân lại yêu Hà
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Phải chăng Tân đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Tân đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu Yêu <33',
+    btnNo: 'Không nhaa :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Vì Hà quá đỗi yêu kiều, quá đỗi xinh đẹp<33333333',
+    mess: 'Mình biết mà 🥰. Yêu Hà nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi mời Hoàng đi ăn kem nha nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
